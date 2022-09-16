4b7d5d04e6fbe7ddc131f2486e8e01273c1794e1
Nguyen Anh Quan <quanna@regit-technology.com>
2022-09-16 10:38:01 +0700

    fix/16092022_TopPage_resolve min-height textarea indent

--- src/components/comment.js
+++ src/components/comment.js
@@ -7,7 +7,7 @@ const Comment = {
       </div>
       <div class="type-row">
         <div class="type-row__avatar"><img class="img-fluid" src="https://res.cloudinary.com/dukoxsuhq/image/upload/v1663232856/Regit%20Demo/avatar2_hbuu1s.png"/></div>
-        <div class="type-row__input"><textarea réi></textarea></div>
+        <div class="type-row__input"><textarea></textarea></div>
       </div>
       <div class="list-comment-row mt-5 mb-5">
         <div class="list-comment-row__avatar"><img class="img-fluid" src="https://res.cloudinary.com/dukoxsuhq/image/upload/v1663232856/Regit%20Demo/avatar2_hbuu1s.png"/></div>
--- src/components/pagination.js
+++ src/components/pagination.js
@@ -2,22 +2,22 @@ const Pagination = {
   render: (page) => {
     return page === "home"
       ? /*html*/ `
-  <div class="pagination--home" id="pagination">
-  <div class="pagination__icon--home"><img src="https://res.cloudinary.com/dukoxsuhq/image/upload/v1663293150/Regit%20Demo/Vector_a7oih2.png" alt="" class="img-fluid"></div>
-      <button class="pagination__button--home active"><span class="pagination__page--home ">1</span></button>
-      <button class="pagination__button--home"><span class="pagination__page--home">2</span></button>
-      <button class="pagination__button--home"><span class="pagination__page--home">3</span></button>
-      <button class="pagination__button--home"><span class="pagination__page--home">4</span></button>
-      <button class="pagination__button--home"><span class="pagination__page--home">5</span></button>
-  <div class="pagination__icon--home"><img src="https://res.cloudinary.com/dukoxsuhq/image/upload/v1663293150/Regit%20Demo/Vector2_moqvkm.png" alt="" class="img-fluid"></div>
-</div>`
+    <div class="pagination--home" id="pagination">
+      <div class="pagination__icon--home"><img src="https://res.cloudinary.com/dukoxsuhq/image/upload/v1663293150/Regit%20Demo/Vector_a7oih2.png" alt="" class="img-fluid"></div>
+          <button class="pagination__button--home active"><span class="pagination__page--home ">1</span></button>
+          <button class="pagination__button--home"><span class="pagination__page--home">2</span></button>
+          <button class="pagination__button--home"><span class="pagination__page--home">3</span></button>
+          <button class="pagination__button--home"><span class="pagination__page--home">4</span></button>
+          <button class="pagination__button--home"><span class="pagination__page--home">5</span></button>
+      <div class="pagination__icon--home"><img src="https://res.cloudinary.com/dukoxsuhq/image/upload/v1663293150/Regit%20Demo/Vector2_moqvkm.png" alt="" class="img-fluid"></div>
+    </div>`
       : /*html*/ `
-  <div class="pagination" id="pagination">
-  <button class="pagination__button active"><span class="pagination__page "></span></button>
-  <button class="pagination__button"><span class="pagination__page"></span></button>
-  <button class="pagination__button"><span class="pagination__page"></span></button>
-  <button class="pagination__button"><span class="pagination__page"></span></button>
-</div>
+    <div class="pagination" id="pagination">
+      <button class="pagination__button active"><span class="pagination__page "></span></button>
+      <button class="pagination__button"><span class="pagination__page"></span></button>
+      <button class="pagination__button"><span class="pagination__page"></span></button>
+      <button class="pagination__button"><span class="pagination__page"></span></button>
+    </div>
   `;
   },
   afterRender: (page) => {
